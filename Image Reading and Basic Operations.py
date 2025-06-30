b57bb3a56e2e2239e09657eb933ddba222b8a83d
EesaYousuf <eesayousufyousuf@gmail.com>
2025-06-30 10:02:44 +0530

    op3

--- Image Reading and Basic Operations.py
+++ Image Reading and Basic Operations.py
@@ -7,3 +7,6 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Show original and grayscale
 cv2.imshow('Original Image', img)
 cv2.imshow('Grayscale Image', gray)
+# Save the grayscale image
+cv2.imwrite('gray_output.jpg', gray)
+
