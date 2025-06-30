413afb2010d695a488ff8631df579bdb90c5e12c
EesaYousuf <eesayousufyousuf@gmail.com>
2025-06-30 10:01:43 +0530

    op2

--- Image Reading and Basic Operations.py
+++ Image Reading and Basic Operations.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as
 # Load image
 img = cv2.imread('sample.jpg')  # Provide your own path
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Show original and grayscale
+cv2.imshow('Original Image', img)
+cv2.imshow('Grayscale Image', gray)
