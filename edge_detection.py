4d1ca59a1a2d5c4478305e9bc7cc80d716aff3f7
EesaYousuf <eesayousufyousuf@gmail.com>
2025-06-30 09:57:15 +0530

    edg11

--- edge_detection.py
+++ edge_detection.py
@@ -7,3 +7,9 @@ img = cv2.imread('images/sample.jpg', cv2.IMREAD_GRAYSCALE)
 sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
 sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
 sobel_combined = cv2.magnitude(sobel_x, sobel_y)
+# 2. Laplacian Edge Detection
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+# 3. Canny Edge Detection
+canny = cv2.Canny(img, 100, 200)
+
