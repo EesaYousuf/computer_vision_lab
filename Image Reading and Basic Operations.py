d387657ab4125cc81ad2af3e8bd4f6e189b4397b
EesaYousuf <eesayousufyousuf@gmail.com>
2025-06-30 10:01:12 +0530

    op1

--- Image Reading and Basic Operations.py
+++ Image Reading and Basic Operations.py
@@ -1,3 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as 
+# Load image
+img = cv2.imread('sample.jpg')  # Provide your own path
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
